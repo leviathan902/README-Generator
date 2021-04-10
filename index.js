6ca3d3176a52194eab158f4e429361a7be7a96c6
Levi H <levih902@gmail.com>
2021-04-10 17:15:29 -0500

    Updated prompts. Updated functions.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const generate = require("/utils/generateMarkdown.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 const fs = require('fs');
-const axios = require("axios");
 
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer
+  .prompt([
+    /* Pass your questions in here */
     {
         type: 'input',
         name: 'title',
@@ -32,18 +33,41 @@ const questions = [
         message: 'Who should be credited in this repo?',
     },
     {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter GitHub username: ',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter email address: ',
+    },
+    {
         type: 'rawlist',
         name: 'license',
         message: ['Apache 2.0', 'BSD 2-Clause', 'BSD 3-Clause', 'GNU AGPLv3.0', 'MIT', 'Mozilla Public 2.0'],
     },
-
-];
+    
+])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+    .then(response => {
+        // Use user feedback for... whatever!!
+        console.log(response);
+        fs.writeFile("README.md"), generateMarkdown(response), (err) => {
+            err ? console.error(err) : console.log(success)
+        }
+      });
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data){
+        writeToFile("README.md", generateMarkdown(data));
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,100 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const noLicense = "";
+
+function renderLicenseBadge(license) {
+  if(License === "MIT") {
+    return `![MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (License === "MOZILLA") {
+    return `![MOZILLA: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`;
+  } else if (License === "IBM") {
+    return `![IBM: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)`;
+  } else if (License === "APACHE") {
+    return `![APACHE](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+  } else {
+    return noLicense;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(License === "MIT") {
+    return `(https://opensource.org/licenses/MIT)`;
+  } else if (License === "MOZILLA") {
+    return `(https://opensource.org/licenses/MPL-2.0)`;
+  } else if (License === "IBM") {
+    return `(https://opensource.org/licenses/IPL-1.0)`;
+  } else if (License === "APACHE") {
+    return `(https://opensource.org/licenses/Apache-2.0)`;
+  } else {
+    return noLicense;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(License === "MIT") {
+    return `[License: MIT]`;
+  } else if (License === "MOZILLA") {
+    return `[License: MPL 2.0]`;
+  } else if (License === "IBM") {
+    return `[License: IPL 1.0]`;
+  } else if (License === "APACHE") {
+    return `[License: APACHE]`;
+  } else {
+    return noLicense;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log("Creating README...")
+  return `# ${renderLicenseBadge(data.license)}
+
+# Description
+${data.Description}
+
+# Table of Contents
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Credit](#credit)
+
+* [Tests](#tests)
+
+* [License](#license)
+
+* [Questions](#questions)
+
+# Installation
+
+Dependancies have to be installed in order to run the application: ${data.Installation}
+
+# Usage
+
+Uses for this application: ${data.Usage}
+
+# Credit
+
+Involved parties: ${data.Credit}
+
+# Tests
+
+In order to run tests on this application, the following command needs to be run: ${data.Test}
+
+# License
+
+This project is licensed under: 
+${renderLicenseSection(data.License)}
+${renderLicenseLink(data.License)}
+
+
+# Questions
+
+For any questions about the application, reach out to: ${data.GitHubUser} or: ${data.Email}.
 
 `;
 }
