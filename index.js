b81ad80cd617c106042a8d6456968fdd7f0e3678
Levi H <levih902@gmail.com>
2021-04-11 18:40:38 -0500

    Update.

--- index.js
+++ index.js
@@ -1,73 +1,71 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown.js");
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 
 // TODO: Create an array of questions for user input
 inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Description of project',
-    },
-    {
-        type: 'confirm',
-        name: 'installation',
-        message: 'What were the steps required to install your project?',
-    },
-    {
-        type: 'confirm',
-        name: 'usage',
-        message: 'What are some examples of usage for this project?',
-    },
-    {
-        type: 'confirm',
-        name: 'credits',
-        message: 'Who should be credited in this repo?',
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'Enter GitHub username: ',
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'Enter email address: ',
-    },
-    {
-        type: 'rawlist',
-        name: 'license',
-        message: ['Apache 2.0', 'BSD 2-Clause', 'BSD 3-Clause', 'GNU AGPLv3.0', 'MIT', 'Mozilla Public 2.0'],
-    },
-    
-])
+    .prompt([
+        /* Pass your questions in here */
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Description of project?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What were the steps required to install your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are some examples of usage for this project?',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Who should be credited in this repo?',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Enter GitHub username: ',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Enter email address: ',
+        },
+        {
+            type: 'list',
+            message: 'Select License:',
+            name: 'license',
+            choices: ['MIT', 'MOZILLA', 'IBM', 'APACHE'],
+        },
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-    .then(response => {
-        // Use user feedback for... whatever!!
+    ])
+
+    // TODO: Create a function to write README file
+    // function writeToFile(fileName, data) {
+    .then((response) => {
         console.log(response);
-        fs.writeFile("README.md"), generateMarkdown(response), (err) => {
-            err ? console.error(err) : console.log(success)
-        }
-      });
+        fs.writeFile('README.md', generateMarkdown(response), (err) => {})
+    });
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     inquirer.prompt(questions)
+//     .then(function(data){
+//         writeToFile("README.md", generateMarkdown(data));
+//     })
 // }
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then(function(data){
-        writeToFile("README.md", generateMarkdown(data));
-    })
-}
-
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,13 +3,13 @@
 const noLicense = "";
 
 function renderLicenseBadge(license) {
-  if(License === "MIT") {
+  if(license === "MIT") {
     return `![MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
-  } else if (License === "MOZILLA") {
+  } else if (license === "MOZILLA") {
     return `![MOZILLA: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`;
-  } else if (License === "IBM") {
+  } else if (license === "IBM") {
     return `![IBM: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)`;
-  } else if (License === "APACHE") {
+  } else if (license === "APACHE") {
     return `![APACHE](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
   } else {
     return noLicense;
@@ -19,13 +19,13 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(License === "MIT") {
+  if(license === "MIT") {
     return `(https://opensource.org/licenses/MIT)`;
-  } else if (License === "MOZILLA") {
+  } else if (license === "MOZILLA") {
     return `(https://opensource.org/licenses/MPL-2.0)`;
-  } else if (License === "IBM") {
+  } else if (license === "IBM") {
     return `(https://opensource.org/licenses/IPL-1.0)`;
-  } else if (License === "APACHE") {
+  } else if (license === "APACHE") {
     return `(https://opensource.org/licenses/Apache-2.0)`;
   } else {
     return noLicense;
@@ -35,13 +35,13 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(License === "MIT") {
+  if(license === "MIT") {
     return `[License: MIT]`;
-  } else if (License === "MOZILLA") {
+  } else if (license === "MOZILLA") {
     return `[License: MPL 2.0]`;
-  } else if (License === "IBM") {
+  } else if (license === "IBM") {
     return `[License: IPL 1.0]`;
-  } else if (License === "APACHE") {
+  } else if (license === "APACHE") {
     return `[License: APACHE]`;
   } else {
     return noLicense;
